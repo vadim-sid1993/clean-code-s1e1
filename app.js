b5180978cd94b4cbdadc251824fcea58fca478b7
vadim-sid1993 <79473969+vadim-sid1993@users.noreply.github.com>
2022-10-31 10:01:16 +0300

    refactor: alternative for media according to rule 1.2 in html-and-css.md advanced level

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement = function (taskString) {
 
     deleteButton.className = "button-delete";
     deleteButtonImg.src = './remove.svg';
+    deleteButtonImg.alt = "deletion picture";
     deleteButton.appendChild(deleteButtonImg);
 
 
